8e85a4cb9a2a476b715d5f0f9b0f40a00c0cfda5
KishoreDevolper <kishore280420@gmail.com>
2023-06-02 18:07:53 +0530

    code update

--- app/configs/config.js
+++ app/configs/config.js
@@ -1,20 +1,28 @@
 module.exports = function (env) {
   const DEV_CONSTANTS = {
     PORT: 2275,
-    MONGO_URI: "mongodb+srv://findoc_user:nQKMemhPm0N4maIJ@cluster0.kr4lh0f.mongodb.net/findoc_communication",
+    MONGO_URI:"mongodb+srv://findoc_user:nQKMemhPm0N4maIJ@cluster0.kr4lh0f.mongodb.net/findoc_communication",
     NODE_ENV: "development",
-    SERVICE_NAME : "Findost-CMS-Service",
-    USER_URL : "http://doodlebluelive.com:2811",
-    200: "success"
+    SERVICE_NAME: "Findost-CMS-Service",
+    USER_URL: "http://doodlebluelive.com:2811",
+    200: "success",
+    USER_URL: process.env.USER_URL,
+    COMMUNICATION_URL: process.env.COMMUNICATION_URL,
+    CMS_URL: process.env.CMS_URL,
+    FINANCE_URL: process.env.FINANCE_URL,
   };
 
   const LOCAL_CONSTANTS = {
     PORT: 2275,
-    MONGO_URI: "mongodb+srv://findoc_user:nQKMemhPm0N4maIJ@cluster0.kr4lh0f.mongodb.net/findoc_communication",
+    MONGO_URI:"mongodb+srv://findoc_user:nQKMemhPm0N4maIJ@cluster0.kr4lh0f.mongodb.net/findoc_communication",
     NODE_ENV: "development",
-    SERVICE_NAME : "Findost-CMS-Service",
-    USER_URL : "http://doodlebluelive.com:2811",
-    200: "success"
+    SERVICE_NAME: "Findost-CMS-Service",
+    USER_URL: "http://doodlebluelive.com:2811",
+    200: "success",
+    USER_URL: process.env.USER_URL,
+    COMMUNICATION_URL: process.env.COMMUNICATION_URL,
+    CMS_URL: process.env.CMS_URL,
+    FINANCE_URL: process.env.FINANCE_URL,
   };
 
   const PROD_CONSTANTS = {
