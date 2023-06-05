2aae63bb16bf1c0933029731cc503051c03b7d93
Gowrisankar <102644027+gowrisankardoodleblue@users.noreply.github.com>
2023-06-05 19:09:59 +0530

    Update list.service.js

--- app/services/list.service.js
+++ app/services/list.service.js
@@ -1,4 +1,7 @@
 const { NotificationTemplate } = require("../models/notificationTemplate");
+const { Notification } = require("../models/notification");
+
+
 const getNotificationTemplateList = async (params) => {
   let data;
   if (params.all) {
@@ -30,6 +33,49 @@ const getNotificationTemplateList = async (params) => {
       .sort({ createdAt: -1 });
   } else {
     data = await NotificationTemplate.find({
+      isDeleted: false,
+    })
+      .skip((params.page - 1) * params.limit)
+      .limit(params.limit)
+      .sort({ createdAt: -1 });
+  }
+  if (data && data.length) {
+    return { status: true, data: data };
+  } else {
+    return { status: false, data: [] };
+  }
+};
+const getNotificationList = async (params) => {
+  let data;
+  if (params.all) {
+    if (params?.search) {
+      data = await Notification.find({
+        isDeleted: false,
+        $or: [
+          { title: { $regex: `${params?.search}`, $options: "i" } },
+          { content: { $regex: `${params?.search}`, $options: "i" } },
+          { type: { $regex: `${params?.search}`, $options: "i" } },
+        ],
+      }).sort({ createdAt: -1 });
+    } else {
+      data = await Notification.find({
+        isDeleted: false,
+      });
+    }
+  } else if (params?.search) {
+    data = await Notification.find({
+      isDeleted: false,
+      $or: [
+        { title: { $regex: `${params?.search}`, $options: "i" } },
+        { content: { $regex: `${params?.search}`, $options: "i" } },
+        { type: { $regex: `${params?.search}`, $options: "i" } },
+      ],
+    })
+      .skip((params.page - 1) * params.limit)
+      .limit(params.limit)
+      .sort({ createdAt: -1 });
+  } else {
+    data = await Notification.find({
       isDeleted: false,
     })
       .skip((params.page - 1) * params.limit)
