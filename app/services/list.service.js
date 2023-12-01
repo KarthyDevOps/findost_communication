5d726a981069a6b044b1315d7d7defef67683094
kishoreDevolper <kishore.t@doodleblue.in>
2023-12-01 12:28:03 +0530

    code update

--- app/services/list.service.js
+++ app/services/list.service.js
@@ -314,7 +314,7 @@ const getMyScheduleList = async (params) => {
       .find(filter)
       .skip((params.page - 1) * params.limit)
       .limit(params.limit)
-      .sort({ isStared: 1 });
+      .sort({ isStarred: 1 });
   }
   data = JSON.parse(JSON.stringify(data));
 
